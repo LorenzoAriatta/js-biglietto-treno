2cfdce1889764486f715b8ef4dd644bcb4fbd72b
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-04 14:54:07 +0100

    aggiunta variabile per calcolo km

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// chiedere all'utente quanti km vuole percorrere
+// const yourName = prompt('Digita il tuo nome'):
+// console.log(yourName):
+
+// const lastName = prompt('Digita il tuo cognome'):
+// console.log(lastName);
+
+const yourAge = prompt('Digita la tua età')
+console.log(yourAge);
+
+const kmUser = prompt('Quanti km desideri fare?')
+console.log(kmUser);
+
+
+
+// prezzo al km 0.21€
+let price = kmUser * 0.21
+
+
+// sconto 20% per <18 anni
+
+
+
+// sconto 40% >= 65 anni
